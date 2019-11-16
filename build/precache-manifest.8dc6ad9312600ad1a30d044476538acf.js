b23a1055ad2f64bb7fda714edcbf7f68e4fabc0d
chippleh1392 <chippleh@neiu.edu>
2019-11-16 15:24:44 -0600

    created production build

---
+++ build/precache-manifest.8dc6ad9312600ad1a30d044476538acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e3ee5e43f43c267ef1868f7f837237",
+    "url": "/chippleh1392/face-recognition-ai/index.html"
+  },
+  {
+    "revision": "8bd6c7e05be56144b3d0",
+    "url": "/chippleh1392/face-recognition-ai/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e9c6726d627af8d2fcf5",
+    "url": "/chippleh1392/face-recognition-ai/static/css/main.38152b1c.chunk.css"
+  },
+  {
+    "revision": "8bd6c7e05be56144b3d0",
+    "url": "/chippleh1392/face-recognition-ai/static/js/2.ac22b4b3.chunk.js"
+  },
+  {
+    "revision": "e9c6726d627af8d2fcf5",
+    "url": "/chippleh1392/face-recognition-ai/static/js/main.7706cb76.chunk.js"
+  },
+  {
+    "revision": "5a3cb0c56683156f4b78",
+    "url": "/chippleh1392/face-recognition-ai/static/js/runtime-main.0ced0d03.js"
+  }
+]);
